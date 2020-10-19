957f8cb23fdfa861543c07299ecb7ecd1b653432
Logist <forwmryslik@mail.ru>
2020-10-19 18:32:25 +0300

    Добавьте файлы проекта.

---
+++ kassa1test/Models/Credit.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace kassa1test.Models
+{
+    public class Credit
+    {
+        int Sum;
+        int CreditTime;
+        double CreditRate;
+        int PayPeriod;
+        DateTime CreditDate;
+        
+        public Credit()
+        {
+
+        }
+    }
+}
